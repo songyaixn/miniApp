a42d1186669b96ef491ea80e67778341818a9f45
songyaixn <33084868+songyaixn@users.noreply.github.com>
2018-09-06 16:30:56 +0800

    新版本

--- MiniApp/pages/infoList/infoList.js
+++ MiniApp/pages/infoList/infoList.js
@@ -3,11 +3,19 @@
 
 Page({
   data: {
-    logs: []
+    logs: [],
+    lists:[]
   },
   onLoad: function () {
-    this.setData({
-    
+    //创建一个测试用数组
+    var mycars = new Array()
+    mycars[0] = "今天心情不错。"
+    mycars[1] = "我找不到我的路。"
+    mycars[2] = "迷茫期应该怎么度过，失去了梦想和咸鱼有什么区别？谁能给我一点建议。"
+    mycars[3] = "我在等你，就好像在飞机场等待一艘船。"
+    var that = this;
+    that.setData({
+      lists: mycars
     })
   },
   toMenuOneInfo:function(){
